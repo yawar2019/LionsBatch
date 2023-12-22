4fc94b2c04d931b3fd0282b88ee26e1d060002d6
LG <LG@DESKTOP-L1958T5>
2023-12-22 22:43:05 +0530

    committed

---
+++ DapperExample/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using Dapper;
+using DapperExample.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DapperExample.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        SqlConnection con = new SqlConnection("Data Source=DESKTOP-L1958T5\\SQLSERVER;Initial Catalog=HarshaDb;Integrated Security=true");
+
+        // GET: Home
+        public ActionResult Index()
+        {
+            //var result = con.Query<EmployeeModel>("SELECT * from EmployeeModels");
+            var result = con.Query<EmployeeModel>("sp_getEmployee",commandType:System.Data.CommandType.StoredProcedure);
+            return View(result);
+        }
+
+        public ActionResult Create()
+        {
+
+            return View();
+        
+        }
+        [HttpPost]
+        public ActionResult Create(EmployeeModel emp)
+        {
+            DynamicParameters parameters = new DynamicParameters();
+            parameters.Add("@EmpName",emp.EmpName);
+            parameters.Add("@EmpSalary",emp.EmpSalary);
+
+            int i = con.Execute("sp_saveEmployee",param: parameters, commandType: System.Data.CommandType.StoredProcedure);
+            if(i>0)
+            {
+                return RedirectToAction("Index");
+
+            }
+            return View();
+
+        }
+    }
+}
---
+++ DapperExample/Models/EmployeeModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DapperExample.Models
+{
+    public class EmployeeModel
+    {
+        public int EmpId { get; set; }
+        public string EmpName { get; set; }
+        public int EmpSalary { get; set; }
+
+    }
+}
